3242ac0e7c74644943b7801e677648717ef53a32
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-09-06 20:41:21 +0100

    setup navigation within the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <Box className="wrapper" sx={{ width: "100%", height: "100%" }}>
       <Box
-        sx={{ display: "flex", flexDirection: "row", p: 2, gridArea: "header" }}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          borderBottom: "1px solid grey",
+        }}
       >
         Monch.io
       </Box>
       <Box component={"main"} sx={{ display: "flex", flexDirection: "row" }}>
-        <Box sx={{ p: 1 }}>Nav</Box>
-        <Box sx={{ flex: 1 }}>
+        <Nav />
+        <Box sx={{ flex: 1, p: 2 }}>
           <Outlet />
         </Box>
       </Box>
